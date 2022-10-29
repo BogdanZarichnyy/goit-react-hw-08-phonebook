dc68c05de5e1961f5f5f2bb043e0ee0f5090ebe9
BogdanZarichnyy <bogdanzarichnyy@gmail.com>
2022-10-29 17:08:05 +0300

    fix css import

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -2,8 +2,6 @@ import { ContactForm } from "components/ContactForm/ContactForm"
 import { ContactList } from "components/ContactList/ContactList";
 import { Filter } from "components/Filter/Filter";
 
-import css from './Contacts.module.css';
-
 export const Contacts = () => { 
     return (
         <>
